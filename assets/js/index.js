b350ec34ac3c11ea7859d309981db7b7371cc7b2
Miraaj Kadam <miraajkadam@gmail.com>
2025-08-10 22:12:41 +0530

    Sample button for darkmode toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,22 @@
 const toggleBtn = document.querySelector('.toggle__btn')
 const toggleBtnIcon = document.querySelector('.toggle__btn i')
 const dropDownMenu = document.querySelector('.dropdown__menu')
+const darkModeToggle = document.getElementById('toggle-darkmode')
+const DARK_MODE_CLASS = 'darkmode'
 
 toggleBtn.addEventListener('click', () => {
   dropDownMenu.classList.toggle('open')
 })
+
+darkModeToggle.addEventListener('click', () => {
+  const bdy = document.getElementsByTagName('body')[0]
+
+  if (!bdy) return
+
+  if (bdy.classList.contains(DARK_MODE_CLASS)) {
+    bdy.classList.remove(DARK_MODE_CLASS)
+  } else {
+    bdy.classList.add(DARK_MODE_CLASS)
+  }
+})
+
